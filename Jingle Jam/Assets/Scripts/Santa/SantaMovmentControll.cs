d90ca4e173cb0d1ba63ad2aaeb2452dab333411b
Safalici <MALICI20@ku.edu.tr>
2022-12-23 18:42:20 +0300

    Added Santa moıvment

---
+++ Jingle Jam/Assets/Scripts/Santa/SantaMovmentControll.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SantaMovmentControll : MonoBehaviour
+{
+    public int constantmovSpeed = 5;
+    [SerializeField] private int speed = 8;
+    private float horizontal;
+    private bool isFacingRight = true;
+
+    [SerializeField] private Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        Face();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(constantmovSpeed, horizontal * speed);
+    }
+
+    private void Face()
+    {
+        if(isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale *= -1;
+            transform.localScale = localScale;
+        }
+    }
+}
