bc536432f2082130933f6e0b665d079c68e68ba4
Irmak Erkol <ierkol18@ku.edu.tr>
2022-12-24 14:33:14 +0300

    santa movement improved

--- Jingle Jam/Assets/Santa/Santa.cs
+++ Jingle Jam/Assets/Santa/Santa.cs
@@ -4,7 +4,7 @@ public class Santa : MonoBehaviour
 {
     public float speed = 10.0f; // The speed at which Santa moves
     public float tilt = 4.0f; // The tilt of the Santa sprite when he moves
-    public float xMin, xMax, yMin, yMax; // The boundaries of the screen
+    public float yMin, yMax; // The boundaries of the screen
     public AudioClip deathSound; // The sound that plays when Santa dies
     public GameObject presentPrefab; // The prefab for the present that Santa drops
     public float presentInterval = 1.0f; // The interval at which Santa drops presents
@@ -43,18 +43,14 @@ public class Santa : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // Get the horizontal and vertical input axes
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
         // Set the velocity of Santa based on the input axes
-        Vector2 movement = new Vector2(moveHorizontal, 0f);
+        Vector2 movement = new Vector2(0.5f, moveVertical);
         rigidbody.velocity = movement * speed;
 
         // Clamp Santa's position to the boundaries of the screen
-        rigidbody.position = new Vector2(
-            Mathf.Clamp(rigidbody.position.x, xMin, xMax),
-            Mathf.Clamp(rigidbody.position.y, yMin, yMax)
-        );
+        rigidbody.position=  new Vector2(rigidbody.position.x, Mathf.Clamp(rigidbody.position.y, yMin, yMax));
 
     }
 }
