41592f0c58a0cfed3eabd9000bb870cfdbf5bbc8
Irmak Erkol <ierkol18@ku.edu.tr>
2022-12-24 21:25:02 +0300

    bugs fixed

--- Jingle Jam/Assets/Grinch/Rock.cs
+++ Jingle Jam/Assets/Grinch/Rock.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using UnityEngine;
+using System;
 
 public class Rock : MonoBehaviour
 {
     public Vector2 velocity; // The velocity of the rock
 
     [SerializeField] Rigidbody2D rb; // The Rigidbody2D component for the rock
+
 
     private void Update()
     {
@@ -21,14 +23,14 @@ public class Rock : MonoBehaviour
         Santa santa = collider.gameObject.GetComponent<Santa>();
         if (santa != null)
         {
+
             // Remove a point from Santa
             ScoreManager.GetInstance().RemovePointFromSanta();
 
             // Add a point to the Grinch
             ScoreManager.GetInstance().GivePointToGrinch();
 
-            Destroy(gameObject);
-
+            Grinch.Fire_onHit();
         }
     }
 
